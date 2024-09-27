a8d3b64996a084a6e8389bbe7b2a922bc2d624cd
kpenkuu-4au <revolva47@gmail.com>
2024-09-27 16:47:46 +0500

    testing dir

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = 111
-second = 1
-third = 11
-if first == second == third:
-    print(3)
-elif not first == second == third:
-    print(0)
-elif first == second != third or first != second == third or first == third != second:
-    print(2)
