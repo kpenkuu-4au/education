4bf45d5c927ecbf061447dce8f96c65810e1235b
kpenkuu-4au <revolva47@gmail.com>
2024-10-29 10:54:32 +0500

    moving

--- module_13/module_13_3.py
+++ module_13/module_13_3.py
@@ -15,6 +15,5 @@ async def start(message):
 async def all_massages(message):
     await message.answer('Введите команду /start, чтобы начать общение.')
 
-
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
