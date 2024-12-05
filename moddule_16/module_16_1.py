0ad198103c6709f46f73f8784ad10217f452c861
kpenkuu-4au <revolva47@gmail.com>
2024-12-05 19:48:01 +0500

    moving

--- moddule_16/module_16_1.py
+++ moddule_16/module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def home_page() -> dict:
-    return {"message": "Главная страница"}
+async def home_page() -> str:
+    return "Главная страница"
 
 
 @app.get('/user/admin')
-async def admin_page() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def admin_page() -> str:
+    return "Вы вошли как администратор"
 
 
 @app.get('/user/{user_id}')
-async def user_root(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def user_root(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get('/user')
-async def user_info(username: str, age: int) -> dict:
-    return {"Информация о пользователе": f"Имя: {username}, Возраст: {age}"}
+async def user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе: Имя: {username}, Возраст: {age}"
